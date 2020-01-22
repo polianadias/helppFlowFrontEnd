2e2937d2b9eabf606cd6ff33ac5005a26cb92c3c
Poliana Dias <polianadias@outlook.com>
2020-01-22 15:43:51 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+    onChange = (event) => {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            console.log(event.target.result)
+        };
+        reader.readAsText(file);
+    }
+
+    render() {
+
+        return (
+
+            <div className="content" >
+                <h1>Helpp Flow Consulting</h1>
+                <div className="file">
+                    <input type="file" accept=".xml" onChange={e =>
+                        this.onChange(e)} />
+                        <button>enviar</button>
+                </div>
+            </ div >
+
+        )
+    }
 }
-
 export default App;
